6d50f0e448fea052034e30fe4080ad2e56a897a0
Ed Warnicke <hagbard@gmail.com>
2020-11-18 06:23:51 -0600

    Add tag chain element
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

---
+++ pkg/networkservice/tag/client.go
@@ -0,0 +1,56 @@
+// Copyright (c) 2020 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tag
+
+import (
+	"context"
+
+	"git.fd.io/govpp.git/api"
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+	"google.golang.org/grpc"
+)
+
+type tagClient struct {
+	ctx     context.Context
+	vppConn api.Connection
+}
+
+// NewClient returns a Client chain element that applies a 'tag' to the vpp interface created
+func NewClient(ctx context.Context, vppConn api.Connection) networkservice.NetworkServiceClient {
+	return &tagClient{
+		ctx:     ctx,
+		vppConn: vppConn,
+	}
+}
+
+func (t *tagClient) Request(ctx context.Context, request *networkservice.NetworkServiceRequest, opts ...grpc.CallOption) (*networkservice.Connection, error) {
+	conn, err := next.Client(ctx).Request(ctx, request, opts...)
+	if err != nil {
+		return nil, err
+	}
+	if err := create(ctx, conn, t.vppConn, true); err != nil {
+		_, _ = t.Close(ctx, conn, opts...)
+		return nil, err
+	}
+	return conn, nil
+}
+
+func (t *tagClient) Close(ctx context.Context, conn *networkservice.Connection, opts ...grpc.CallOption) (*empty.Empty, error) {
+	return next.Client(ctx).Close(ctx, conn, opts...)
+}
---
+++ pkg/networkservice/tag/common.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2020 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tag
+
+import (
+	"context"
+	"time"
+
+	"git.fd.io/govpp.git/api"
+	interfaces "github.com/edwarnicke/govpp/binapi/interface"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/trace"
+	"github.com/pkg/errors"
+
+	"github.com/networkservicemesh/sdk-vpp/pkg/tools/ifindex"
+)
+
+func create(ctx context.Context, conn *networkservice.Connection, vppConn api.Connection, isClient bool) error {
+	swIfIndex, ok := ifindex.Load(ctx, isClient)
+	if ok {
+		return nil
+	}
+
+	now := time.Now()
+	if _, err := interfaces.NewServiceClient(vppConn).SwInterfaceTagAddDel(ctx, &interfaces.SwInterfaceTagAddDel{
+		SwIfIndex: swIfIndex,
+		Tag:       conn.GetId(),
+	}); err != nil {
+		return errors.WithStack(err)
+	}
+	trace.Log(ctx).
+		WithField("swIfIndex", swIfIndex).
+		WithField("tag", conn.GetId()).
+		WithField("duration", time.Since(now)).
+		WithField("vppapi", "SwInterfaceTagAddDel").Debug("completed")
+	return nil
+}
