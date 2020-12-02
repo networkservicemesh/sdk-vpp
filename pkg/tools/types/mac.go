b7beeb8cbb691ba41bb8be83005d248bcabb7593
Ed Warnicke <hagbard@gmail.com>
2020-12-02 13:26:04 -0600

    Add simple tool to convert net.HardwareAddr to vpp ethernet_types.MacAddress (#38)
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

---
+++ pkg/tools/types/mac.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2020 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import (
+	"net"
+
+	"github.com/edwarnicke/govpp/binapi/ethernet_types"
+)
+
+// ToVppMacAddress - converts *net.HardwareAddr to ethernet_types.MacAddress
+func ToVppMacAddress(hardwareAddr *net.HardwareAddr) ethernet_types.MacAddress {
+	hwAddr := [6]uint8{}
+	copy(hwAddr[:], *hardwareAddr)
+	return ethernet_types.MacAddress(hwAddr)
+}
