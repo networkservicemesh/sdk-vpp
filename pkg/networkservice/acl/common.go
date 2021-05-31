ef8cd69be7e46fb39eef5b91e79c3596cd1c3ef7
Mikhail Avramenko <avramenkomihail15@gmail.com>
2021-05-31 16:39:40 +0700

    added acl chain element
    Signed-off-by: Mikhail Avramenko <avramenkomihail15@gmail.com>

---
+++ pkg/networkservice/acl/common.go
@@ -0,0 +1,95 @@
+// Copyright (c) 2021 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package acl
+
+import (
+	"context"
+	"time"
+
+	"git.fd.io/govpp.git/api"
+	"github.com/edwarnicke/govpp/binapi/acl"
+	"github.com/edwarnicke/govpp/binapi/acl_types"
+	"github.com/edwarnicke/govpp/binapi/interface_types"
+	"github.com/pkg/errors"
+
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
+)
+
+const (
+	aclTag = "nsm-acl-from-config"
+)
+
+func create(ctx context.Context, vppConn api.Connection, tag string, swIfIndex interface_types.InterfaceIndex, aRules []acl_types.ACLRule) ([]uint32, error) {
+	interfaceACLList := &acl.ACLInterfaceSetACLList{
+		SwIfIndex: swIfIndex,
+	}
+
+	var err error
+	interfaceACLList.Acls, err = addACLToACLList(ctx, vppConn, tag, false, aRules)
+	if err != nil {
+		log.FromContext(ctx).Info("ACL_SERVER: error adding acl to acl list ingress")
+		return nil, errors.WithStack(err)
+	}
+	interfaceACLList.NInput = uint8(len(interfaceACLList.Acls))
+
+	egressACLIndeces, err := addACLToACLList(ctx, vppConn, tag, true, aRules)
+	if err != nil {
+		log.FromContext(ctx).Info("ACL_SERVER: error adding acl to acl list egress")
+		return nil, errors.WithStack(err)
+	}
+	interfaceACLList.Acls = append(interfaceACLList.Acls, egressACLIndeces...)
+	interfaceACLList.Count = uint8(len(interfaceACLList.Acls))
+
+	_, err = acl.NewServiceClient(vppConn).ACLInterfaceSetACLList(ctx, interfaceACLList)
+	if err != nil {
+		log.FromContext(ctx).Info("ACL_SERVER: error setting acl list for interface")
+		return nil, errors.WithStack(err)
+	}
+	return interfaceACLList.Acls, nil
+}
+
+func addACLToACLList(ctx context.Context, vppConn api.Connection, tag string, egress bool, aRules []acl_types.ACLRule) ([]uint32, error) {
+	var ACLIndeces []uint32
+
+	now := time.Now()
+	rsp, err := acl.NewServiceClient(vppConn).ACLAddReplace(ctx, aclAdd(tag, egress, aRules))
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	log.FromContext(ctx).
+		WithField("aclIndices", rsp.ACLIndex).
+		WithField("duration", time.Since(now)).
+		WithField("vppapi", "ACLAddReplace").Debug("completed")
+	ACLIndeces = append([]uint32{rsp.ACLIndex}, ACLIndeces...)
+
+	return ACLIndeces, nil
+}
+
+func aclAdd(tag string, egress bool, aRules []acl_types.ACLRule) *acl.ACLAddReplace {
+	aclAddReplace := &acl.ACLAddReplace{
+		ACLIndex: ^uint32(0),
+		Tag:      tag,
+		Count:    uint32(len(aRules)),
+		R:        aRules,
+	}
+	if egress {
+		for _, a := range aRules {
+			a.SrcPrefix, a.DstPrefix = a.DstPrefix, a.SrcPrefix
+		}
+	}
+	return aclAddReplace
+}
---
+++ pkg/networkservice/acl/server.go
@@ -0,0 +1,84 @@
+// Copyright (c) 2021 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package acl provides chain elements for setting acl rules
+package acl
+
+import (
+	"context"
+
+	"git.fd.io/govpp.git/api"
+	"github.com/edwarnicke/govpp/binapi/acl"
+	"github.com/edwarnicke/govpp/binapi/acl_types"
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/memif"
+
+	"github.com/networkservicemesh/sdk-vpp/pkg/tools/ifindex"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/metadata"
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
+)
+
+type aclServer struct {
+	vppConn    api.Connection
+	aclRules   []acl_types.ACLRule
+	aclIndices []uint32
+}
+
+// NewServer creates a NetworkServiceServer chain element to set the ACL on a vpp interface
+func NewServer(vppConn api.Connection, aclrules []acl_types.ACLRule) networkservice.NetworkServiceServer {
+	return &aclServer{
+		vppConn:    vppConn,
+		aclRules:   aclrules,
+		aclIndices: make([]uint32, 0),
+	}
+}
+
+func (a *aclServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	conn, err := next.Server(ctx).Request(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	if request.GetConnection().GetMechanism().Type == memif.MECHANISM && len(a.aclRules) > 0 {
+		swIfIndex, ok := ifindex.Load(ctx, metadata.IsClient(a))
+		if !ok {
+			log.FromContext(ctx).Infof("ACL_SERVER: sw if index for client not found")
+		}
+
+		var indices []uint32
+		if indices, err = create(ctx, a.vppConn, aclTag, swIfIndex, a.aclRules); err != nil {
+			return nil, err
+		}
+		a.aclIndices = indices
+	}
+
+	return conn, nil
+}
+
+func (a *aclServer) Close(ctx context.Context, conn *networkservice.Connection) (*empty.Empty, error) {
+	for ind := range a.aclIndices {
+		_, err := acl.NewServiceClient(a.vppConn).ACLDel(ctx, &acl.ACLDel{ACLIndex: uint32(ind)})
+		if err != nil {
+			log.FromContext(ctx).Infof("ACL_SERVER: error deleting acls")
+		}
+	}
+
+	return next.Server(ctx).Close(ctx, conn)
+}
