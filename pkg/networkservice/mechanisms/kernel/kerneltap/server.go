5bad9c0a9d61b07d967f32fc769b5f765d0db36a
Danil Uzlov <DanilUzlov@yandex.ru>
2021-07-21 17:04:34 +0700

    add logs for delete operation into kerneltap server
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- pkg/networkservice/mechanisms/kernel/kerneltap/server.go
+++ pkg/networkservice/mechanisms/kernel/kerneltap/server.go
@@ -23,8 +23,10 @@ import (
 
 	"git.fd.io/govpp.git/api"
 	"github.com/golang/protobuf/ptypes/empty"
+
 	"github.com/networkservicemesh/api/pkg/api/networkservice"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
 
 	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/metadata"
 )
@@ -53,6 +55,9 @@ func (k *kernelTapServer) Request(ctx context.Context, request *networkservice.N
 }
 
 func (k *kernelTapServer) Close(ctx context.Context, conn *networkservice.Connection) (*empty.Empty, error) {
-	_ = del(ctx, conn, k.vppConn, metadata.IsClient(k))
+	err := del(ctx, conn, k.vppConn, metadata.IsClient(k))
+	if err != nil {
+		log.FromContext(ctx).Error(err)
+	}
 	return next.Server(ctx).Close(ctx, conn)
 }
