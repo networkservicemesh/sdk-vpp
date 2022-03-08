bb5ec7ddeacc83ab78fd26a81db07371a376e3d4
Laszlo Kiraly <laszlo.kiraly@est.tech>
2022-03-08 15:19:59 +0100

    Set the same MTU for the NSC internal interface as the base interface has
    Closes: #514
    
    Signed-off-by: Laszlo Kiraly <laszlo.kiraly@est.tech>

---
+++ pkg/networkservice/mechanisms/vlan/mtu/common.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2022 Nordix Foundation.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mtu
+
+import (
+	"context"
+	"time"
+
+	"git.fd.io/govpp.git/api"
+	"github.com/pkg/errors"
+
+	interfaces "github.com/edwarnicke/govpp/binapi/interface"
+	"github.com/edwarnicke/govpp/binapi/interface_types"
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
+)
+
+func getMTU(ctx context.Context, vppConn api.Connection, swIfIndex interface_types.InterfaceIndex) (uint32, error) {
+	now := time.Now()
+	dc, err := interfaces.NewServiceClient(vppConn).SwInterfaceDump(ctx, &interfaces.SwInterfaceDump{
+		SwIfIndex: swIfIndex,
+	})
+	if err != nil {
+		return 0, errors.Wrapf(err, "error attempting to get interface dump client to determine MTU for swIfIndex %d", swIfIndex)
+	}
+	defer func() { _ = dc.Close() }()
+
+	details, err := dc.Recv()
+	if err != nil {
+		return 0, errors.Wrapf(err, "error attempting to get interface details to determine MTU for swIfIndex %d", swIfIndex)
+	}
+	log.FromContext(ctx).
+		WithField("swIfIndex", swIfIndex).
+		WithField("details.LinkMtu", details.LinkMtu).
+		WithField("duration", time.Since(now)).
+		WithField("vppapi", "SwInterfaceDump").Debug("completed")
+	return uint32(details.LinkMtu), nil
+}
