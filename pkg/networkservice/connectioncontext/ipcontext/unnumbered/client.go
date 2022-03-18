652ab0bc2621413d6a5ad79acf31cdd3b51d241c
Denis Tingaikin <49399980+denis-tingaikin@users.noreply.github.com>
2022-03-18 03:02:18 +0300

    feat: add unnumbered chain element (#533)
    * Add unnumbered chain element
    
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>
    
    * don't depend on loopback chain element
    
    Signed-off-by: denis-tingaikin <denis.tingajkin@xored.com>
    
    Co-authored-by: Artem Glazychev <artem.glazychev@xored.com>

---
+++ pkg/networkservice/connectioncontext/ipcontext/unnumbered/client.go
@@ -0,0 +1,73 @@
+// Copyright (c) 2022 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package unnumbered
+
+import (
+	"context"
+
+	"github.com/edwarnicke/govpp/binapi/interface_types"
+	"github.com/pkg/errors"
+
+	"git.fd.io/govpp.git/api"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/metadata"
+	"github.com/networkservicemesh/sdk/pkg/tools/postpone"
+)
+
+type unnumberedClient struct {
+	vppConn     api.Connection
+	loadIfaceFn func(ctx context.Context, isClient bool) (value interface_types.InterfaceIndex, ok bool)
+}
+
+// NewClient creates a new instance of unnumbered client
+func NewClient(vppConn api.Connection, loadIfaceFn func(ctx context.Context, isClient bool) (value interface_types.InterfaceIndex, ok bool)) networkservice.NetworkServiceClient {
+	return &unnumberedClient{
+		vppConn:     vppConn,
+		loadIfaceFn: loadIfaceFn,
+	}
+}
+
+func (u *unnumberedClient) Request(ctx context.Context, request *networkservice.NetworkServiceRequest, opts ...grpc.CallOption) (*networkservice.Connection, error) {
+	postponeCtxFunc := postpone.ContextWithValues(ctx)
+
+	conn, err := next.Client(ctx).Request(ctx, request, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := addDel(ctx, u.vppConn, metadata.IsClient(u), true, u.loadIfaceFn); err != nil {
+		closeCtx, cancelClose := postponeCtxFunc()
+		defer cancelClose()
+
+		if _, closeErr := u.Close(closeCtx, conn, opts...); closeErr != nil {
+			err = errors.Wrapf(err, "connection closed with error: %s", closeErr.Error())
+		}
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func (u *unnumberedClient) Close(ctx context.Context, conn *networkservice.Connection, opts ...grpc.CallOption) (*empty.Empty, error) {
+	delete(ctx, metadata.IsClient(u))
+	return next.Client(ctx).Close(ctx, conn, opts...)
+}
---
+++ pkg/networkservice/connectioncontext/ipcontext/unnumbered/server.go
@@ -0,0 +1,70 @@
+// Copyright (c) 2022 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package unnumbered
+
+import (
+	"context"
+
+	"git.fd.io/govpp.git/api"
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/pkg/errors"
+
+	"github.com/edwarnicke/govpp/binapi/interface_types"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/metadata"
+	"github.com/networkservicemesh/sdk/pkg/tools/postpone"
+)
+
+type unnumberedServer struct {
+	vppConn     api.Connection
+	loadIfaceFn func(ctx context.Context, isClient bool) (value interface_types.InterfaceIndex, ok bool)
+}
+
+// NewServer creates a new instance of unnumbered server
+func NewServer(vppConn api.Connection, loadIfaceFn func(ctx context.Context, isClient bool) (value interface_types.InterfaceIndex, ok bool)) networkservice.NetworkServiceServer {
+	return &unnumberedServer{
+		vppConn:     vppConn,
+		loadIfaceFn: loadIfaceFn,
+	}
+}
+
+func (u *unnumberedServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	postponeCtxFunc := postpone.ContextWithValues(ctx)
+
+	conn, err := next.Server(ctx).Request(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := addDel(ctx, u.vppConn, metadata.IsClient(u), true, u.loadIfaceFn); err != nil {
+		closeCtx, cancelClose := postponeCtxFunc()
+		defer cancelClose()
+
+		if _, closeErr := u.Close(closeCtx, conn); closeErr != nil {
+			err = errors.Wrapf(err, "connection closed with error: %s", closeErr.Error())
+		}
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func (u *unnumberedServer) Close(ctx context.Context, conn *networkservice.Connection) (*empty.Empty, error) {
+	delete(ctx, metadata.IsClient(u))
+	return next.Server(ctx).Close(ctx, conn)
+}
