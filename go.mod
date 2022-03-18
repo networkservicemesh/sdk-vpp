9984c7c6df6eddcd22aee359eb18e5e64e116fa0
Artem Glazychev <artem.glazychev@xored.com>
2022-03-18 15:04:18 +0700

    Add options to the forwarder to use it with an external vpp (#530)
    * Add options to the forwarder to use it with an external vpp
    
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>
    
    * Add default values
    
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

--- go.mod
+++ go.mod
@@ -7,9 +7,10 @@ require (
 	github.com/edwarnicke/govpp v0.0.0-20220311182453-f32f292e0e91
 	github.com/edwarnicke/serialize v1.0.7
 	github.com/golang/protobuf v1.5.2
+	github.com/google/uuid v1.1.2
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/networkservicemesh/api v1.2.1-0.20220315001249-f33f8c3f2feb
-	github.com/networkservicemesh/sdk v0.5.1-0.20220316101237-288caa7bbc1c
+	github.com/networkservicemesh/sdk v0.5.1-0.20220316105041-b88289b9022e
 	github.com/networkservicemesh/sdk-kernel v0.0.0-20220316101641-0103343013f0
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
---
+++ pkg/networkservice/chains/forwarder/options.go
@@ -0,0 +1,113 @@
+// Copyright (c) 2022 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build linux
+
+package forwarder
+
+import (
+	"net/url"
+	"time"
+
+	"google.golang.org/grpc"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/cleanup"
+
+	"github.com/networkservicemesh/sdk-vpp/pkg/networkservice/mechanisms/vxlan"
+	"github.com/networkservicemesh/sdk-vpp/pkg/networkservice/stats"
+)
+
+type forwarderOptions struct {
+	name            string
+	authorizeServer networkservice.NetworkServiceServer
+	clientURL       *url.URL
+	dialTimeout     time.Duration
+	domain2Device   map[string]string
+	statsOpts       []stats.Option
+	cleanupOpts     []cleanup.Option
+	vxlanOpts       []vxlan.Option
+	dialOpts        []grpc.DialOption
+}
+
+// Option is an option pattern for forwarder chain elements
+type Option func(o *forwarderOptions)
+
+// WithName - set a forwarder name
+func WithName(name string) Option {
+	return func(o *forwarderOptions) {
+		o.name = name
+	}
+}
+
+// WithAuthorizeServer sets authorization server chain element
+func WithAuthorizeServer(authorizeServer networkservice.NetworkServiceServer) Option {
+	if authorizeServer == nil {
+		panic("Authorize server cannot be nil")
+	}
+	return func(o *forwarderOptions) {
+		o.authorizeServer = authorizeServer
+	}
+}
+
+// WithClientURL sets clientURL.
+func WithClientURL(clientURL *url.URL) Option {
+	return func(c *forwarderOptions) {
+		c.clientURL = clientURL
+	}
+}
+
+// WithDialTimeout sets dial timeout for the client
+func WithDialTimeout(dialTimeout time.Duration) Option {
+	return func(o *forwarderOptions) {
+		o.dialTimeout = dialTimeout
+	}
+}
+
+// WithVlanDomain2Device sets vlan option
+func WithVlanDomain2Device(domain2Device map[string]string) Option {
+	return func(o *forwarderOptions) {
+		o.domain2Device = domain2Device
+	}
+}
+
+// WithStatsOptions sets stats options
+func WithStatsOptions(opts ...stats.Option) Option {
+	return func(o *forwarderOptions) {
+		o.statsOpts = opts
+	}
+}
+
+// WithCleanupOptions sets cleanup options
+func WithCleanupOptions(opts ...cleanup.Option) Option {
+	return func(o *forwarderOptions) {
+		o.cleanupOpts = opts
+	}
+}
+
+// WithVxlanOptions sets vxlan options
+func WithVxlanOptions(opts ...vxlan.Option) Option {
+	return func(o *forwarderOptions) {
+		o.vxlanOpts = opts
+	}
+}
+
+// WithDialOptions sets dial options
+func WithDialOptions(opts ...grpc.DialOption) Option {
+	return func(o *forwarderOptions) {
+		o.dialOpts = opts
+	}
+}
