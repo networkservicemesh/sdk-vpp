5db615624ec517c329006a13bbfe1b55db818303
Nikita Skrynnik <93182827+NikitaSkrynnik@users.noreply.github.com>
2024-08-28 23:47:52 +1100

    Add printing of WireguardPeerEvent events (#850)
    * Add printing of WireguardPeerEvent events
    
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>
    
    * apply review comments
    
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>
    
    ---------
    
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- pkg/networkservice/up/peerup/common.go
+++ pkg/networkservice/up/peerup/common.go
@@ -89,7 +89,7 @@ func waitForPeerUp(ctx context.Context, vppConn api.Connection, pubKey string, i
 			log.FromContext(ctx).Infof("failed to get any WireguardPeerEvents: %s", ctx.Err())
 			return errors.Wrap(ctx.Err(), "provided context is done")
 		case rawMsg := <-watcher.Events():
-			log.FromContext(ctx).Infof("got WireguardPeerEvent: %v, %v", rawMsg.GetMessageName(), rawMsg.GetMessageType())
+			log.FromContext(ctx).Infof("received WireguardPeerEvent: %v", rawMsg)
 			if msg, ok := rawMsg.(*wireguard.WireguardPeerEvent); ok &&
 				msg.PeerIndex == peerIndex &&
 				msg.Flags&wireguard.WIREGUARD_PEER_ESTABLISHED != 0 {
